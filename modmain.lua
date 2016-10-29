f8eb00b842124421a16234f25f3f7eae6eee2be3
Paul Gibbs <djpaul@gmail.com>
2016-10-29 14:07:20 +0100

    Proof of concept of sorting - pickaxe before axe

--- modmain.lua
+++ modmain.lua
@@ -6,4 +6,11 @@ GLOBAL.require("debugtools")
 
 AddClassPostConstruct("screens/playerhud", function(self)
 	print("helllo world")
+	for k, v in pairs(GLOBAL.AllRecipes) do
+		if (v.name == 'pickaxe') then
+			v.sortkey = 1
+		end
+	end
+
+	print('DJPAUL')
 end)
