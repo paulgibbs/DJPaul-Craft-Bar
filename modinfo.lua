5609d4b1ed2bb7a8e804f158e778e6951f7fcec9
Paul Gibbs <djpaul@gmail.com>
2016-10-29 12:32:22 +0100

    Add plugin headers

---
+++ modinfo.lua
@@ -0,0 +1,21 @@
+name        = "DJPaul's Improved Craft Bar"
+description = "TODO"
+author      = "Paul Gibbs (DJPaul)"
+version     = "0.1"
+forumthread = "/topic/TODO"
+
+api_version                = 10  -- DST api version
+dont_starve_compatible     = false
+reign_of_giants_compatible = false
+shipwrecked_compatible     = false
+dst_compatible             = true
+priority                   = 0  -- Relative load order
+server_filter_tags         = { "djpaul", "craft bar" }
+icon_atlas                 = "modicon.xml"
+icon                       = "modicon.tex"
+
+client_only_mod         = true
+all_clients_require_mod = false
+
+configuration_options = {
+}
